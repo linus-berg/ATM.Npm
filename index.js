dac4d5494d79630068c78b0310156951ede8fc7b
linusberg <linus@fenix.me.uk>
2022-10-09 17:20:35 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+var ChangesStream = require('changes-stream');
+var axios = require('axios');
+
+var changes = new ChangesStream({
+  db: 'https://replicate.npmjs.com/registry',
+  include_docs: true,
+  since: 'now'
+});
+
+changes.on('data', function (change) {
+  var name = change.doc.name
+  if(name){
+    console.log(name);
+    PostToTracker(name);
+  }
+});
+
+const PostToTracker = async (artifact) => {
+  try {
+    const res = await axios.post("http://localhost:4001/api/artifact/track", {
+      module: 'npm',
+      artifact: artifact
+    }); 
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
+};
